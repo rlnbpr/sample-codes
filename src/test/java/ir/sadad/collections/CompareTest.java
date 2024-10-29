27249d0f3baa133c432a9a19f5950c8cd936daff
a.nayebpour <a.nayebpour@sadad.co.ir>
2024-10-29 14:42:36 +0000

    compare test

--- src/test/java/ir/sadad/collections/CompareTest.java
+++ src/test/java/ir/sadad/collections/CompareTest.java
@@ -2,6 +2,8 @@ package ir.sadad.collections;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 
 class CompareTest {
 
@@ -19,5 +21,24 @@ class CompareTest {
         String str0 = "zero";
         String str1 = "one";
         assert str0.compareTo(str1) > 0;
+    }
+
+    @Test
+    void compareDouble() {
+        Double aDouble = 1.0;
+        Double bDouble = 1.0;
+//        System.out.println(aDouble == bDouble);
+        assert aDouble != bDouble;
+        assert aDouble.equals(bDouble);
+        assert aDouble.compareTo(bDouble) == 0;
+    }
+
+    @Test
+    void compareBigDecimal() {
+        BigDecimal bigDecimal1 = BigDecimal.valueOf(1);
+        BigDecimal bigDecimal2 = BigDecimal.valueOf(1.000);
+        assert bigDecimal1 != bigDecimal2;
+        assert !bigDecimal2.equals(bigDecimal1);
+        assert bigDecimal2.compareTo(bigDecimal1) == 0;
     }
 }
